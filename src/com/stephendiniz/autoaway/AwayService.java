7b8f02bf9d9f927beed6c2c3b74cb9fad2fb2920
Andrew Simmons <lamborghini217@gmail.com>
2011-08-01 23:22:32 -0400

    Changed the file but didn't test.

--- src/com/stephendiniz/autoaway/AwayService.java
+++ src/com/stephendiniz/autoaway/AwayService.java
@@ -18,58 +18,67 @@ public class AwayService extends Service
 	@Override
 	public IBinder onBind(Intent i)
 	{ return null; }
+	
+	public BroadcastReceiver smsreceiver;
+	 
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate();
 
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate();
-        
-        registerReceiver(smsreceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
-    }
-    
-    private BroadcastReceiver smsreceiver = new BroadcastReceiver()
-    {
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-            Bundle bundle = intent.getExtras();        
-            SmsMessage[] msgs = null;
-            
-            if(null != bundle)
-            {
-            	String info = null;
-                Object[] pdus = (Object[]) bundle.get("pdus");
-                msgs = new SmsMessage[pdus.length];
-                
-                for (int i = 0; i < msgs.length; i++)
-                {
-                    msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-                    info = msgs[i].getOriginatingAddress();
-                }
+		this.smsreceiver = new BroadcastReceiver()
+		{
+			@Override
+			public void onReceive(Context context, Intent intent)
+			{
+				Bundle bundle = intent.getExtras();        
+				SmsMessage[] msgs = null;
 
-                Main mo = new Main();
-                sendSms(info, mo.messageContent);
-            }
-        }
-    };
-    
-    private void sendSms(String phonenumber, String message)
-    {
-        SmsManager manager = SmsManager.getDefault();
-        
-        PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
-        PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
-        
-        int length = message.length();
-        
-        if(length > 160)
-        {
-                ArrayList<String> messagelist = manager.divideMessage(message);
-                
-                manager.sendMultipartTextMessage(phonenumber, null, messagelist, null, null);
-        }
-        else
-        {
-                manager.sendTextMessage(phonenumber, null, message, piSend, piDelivered);
-        }
-    }
+				if(null != bundle)
+				{
+					String info = null;
+					Object[] pdus = (Object[]) bundle.get("pdus");
+					msgs = new SmsMessage[pdus.length];
+
+					for (int i = 0; i < msgs.length; i++)
+					{
+						msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+						info = msgs[i].getOriginatingAddress();
+					}
+
+					Main mo = new Main();
+					sendSms(info, mo.messageContent);
+				}
+			}
+		};
+
+		registerReceiver(this.smsreceiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+	}
+
+
+	private void sendSms(String phonenumber, String message)
+	{
+		SmsManager manager = SmsManager.getDefault();
+
+		PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+		PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+
+		int length = message.length();
+
+		if (length > 160)
+		{
+			ArrayList<String> messagelist = manager.divideMessage(message);
+
+			manager.sendMultipartTextMessage(phonenumber, null, messagelist, null, null);
+		}
+		else
+		{
+			manager.sendTextMessage(phonenumber, null, message, piSend, piDelivered);
+		}
+	}
+	
+	public void onDestroy() {
+		super.onDestroy();
+		this.unregisterReceiver(this.smsreceiver);
+	}
 }
+
