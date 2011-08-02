360e2b6c0887eb8fac09c392f7f83eda295fd0e0
Andrew Simmons <lamborghini217@gmail.com>
2011-08-02 14:13:49 -0400

    Added an SMS listener file, no idea how to use it

---
+++ src/com/stephendiniz/autoaway/SmsListener.java
@@ -0,0 +1,62 @@
+package com.stephendiniz.autoaway;
+
+import java.util.ArrayList;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+
+public class SmsListener extends BroadcastReceiver {
+	
+	public BroadcastReceiver smsreceiver;
+			@Override
+			public void onReceive(Context context, Intent intent)
+			{
+				Bundle bundle = intent.getExtras();        
+				SmsMessage[] msgs = null;
+
+				if(null != bundle)
+				{
+					String info = null;
+					Object[] pdus = (Object[]) bundle.get("pdus");
+					msgs = new SmsMessage[pdus.length];
+
+					for (int i = 0; i < msgs.length; i++)
+					{
+						msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+						info = msgs[i].getOriginatingAddress();
+					}
+
+					Main mo = new Main();
+					sendSms(info, mo.messageContent);
+				}
+			};
+
+
+	private void sendSms(String phonenumber, String message)
+	{
+		SmsManager manager = SmsManager.getDefault();
+
+		PendingIntent piSend = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+		PendingIntent piDelivered = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+
+		int length = message.length();
+
+		if (length > 160)
+		{
+			ArrayList<String> messagelist = manager.divideMessage(message);
+
+			manager.sendMultipartTextMessage(phonenumber, null, messagelist, null, null);
+		}
+		else
+		{
+			manager.sendTextMessage(phonenumber, null, message, piSend, piDelivered);
+		}
+	}
+}
+}
