9e1a465524e92350b10d6032d24d21bea6e55586
Stephen Diniz <DizzyThermal@gmail.com>
2011-08-04 23:13:52 -0400

    Added features to Preference Activity

--- src/com/stephendiniz/autoaway/Main.java
+++ src/com/stephendiniz/autoaway/Main.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -16,23 +17,27 @@ import android.preference.PreferenceManager;
 public class Main extends PreferenceActivity implements OnPreferenceChangeListener
 {
 	final String SERVICE_PREF	= "serviceCheckBox";
+	final String SILENT_PREF	= "silentCheckBox";
 	final String MESSAGE_PREF	= "messageEditText";
+	final String INFORM_PREF	= "informCheckBox";
 	final String DELAY_PREF		= "delayEditText";
+	final String REPEAT_PREF	= "repeatCheckBox";
 	
 	final int NOTIFICATION_ID	= 1;
-
-	private boolean	serviceRunning;
-	private String	messageContent;
-	private int		delayDuration;
 	
-	Resources r = getResources();
+	Resources r;
 	
-	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-	SharedPreferences.Editor editor = prefs.edit();
+	AudioManager aManager;
+	
+	SharedPreferences prefs;
+	SharedPreferences.Editor editor;
 	
 	Preference serviceCheckBox;
+	Preference silentCheckBox;
 	Preference messageEditText;
+	Preference informCheckBox;
 	Preference delayEditText;
+	Preference repeatCheckBox;
 	
 	@Override
 	public void onCreate (Bundle savedInstanceState)
@@ -44,46 +49,60 @@ public class Main extends PreferenceActivity implements OnPreferenceChangeListen
 		serviceCheckBox = (Preference)findPreference(SERVICE_PREF);
 		serviceCheckBox.setOnPreferenceChangeListener(this);
 		
+		silentCheckBox = (Preference)findPreference(SILENT_PREF);
+		silentCheckBox.setOnPreferenceChangeListener(this);
+		
 		messageEditText = (Preference)findPreference(MESSAGE_PREF);
 		messageEditText.setOnPreferenceChangeListener(this);
+		
+		informCheckBox = (Preference)findPreference(INFORM_PREF);
+		informCheckBox.setOnPreferenceChangeListener(this);
 		
 		delayEditText = (Preference)findPreference(DELAY_PREF);
 		delayEditText.setOnPreferenceChangeListener(this);
 		
-		//onCreate, service shouldn't be running
-		setServiceStatus(false);
+		repeatCheckBox = (Preference)findPreference(REPEAT_PREF);
+		repeatCheckBox.setOnPreferenceChangeListener(this);
+		
+		r = getResources();
+		
+		aManager = (AudioManager)getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+		
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		editor = prefs.edit();
 	}
 	
 	public void onResume()
 	{
 		super.onResume();
 		
+		if(aManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT)
+			setSilentStatus(true);
+		else
+			setSilentStatus(false);
+		
+		setServiceStatus(prefs.getBoolean(SERVICE_PREF, false));
+		setMessageContent(prefs.getString(MESSAGE_PREF, r.getString(R.string.message_content)));
+		setInformStatus(prefs.getBoolean(INFORM_PREF, true));
+		setDelayDuration(prefs.getString(DELAY_PREF, "30"));
+		setRepeatStatus(prefs.getBoolean(REPEAT_PREF, false));
+		
 		if(serviceRunning())
 			setPreferenceStatus(false);
-		
-		setMessageContent(prefs.getString(MESSAGE_PREF, r.getString(R.string.message_content)));
-		setDelayDuration(prefs.getString(DELAY_PREF, "30"));
 	}
 	
-	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState)
+	public void onPause()
 	{
-		super.onSaveInstanceState(savedInstanceState);
+		super.onPause();
 		
-		//Save information as Strings
-		savedInstanceState.putString("messageSaved", getMessageContent());
-		savedInstanceState.putString("delaySaved", Integer.toString(getDelayDuration()));
+		editor.putBoolean(SERVICE_PREF, serviceRunning());
+		editor.putBoolean(SILENT_PREF, isSilent());
+		editor.putString(MESSAGE_PREF, getMessageContent());
+		editor.putBoolean(INFORM_PREF, getInformStatus());
+		editor.putString(DELAY_PREF, Integer.toString(getDelayDuration()));
+		editor.putBoolean(REPEAT_PREF, getRepeatStatus());
 	}
-	
-    @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState)
-    {
-		super.onRestoreInstanceState(savedInstanceState);
-		
-		//Restore and set saved information from Bundle as correct types
-		setMessageContent(savedInstanceState.getString("messageSaved"));
-		setDelayDuration(savedInstanceState.getString("delaySaved"));
-    }
 	
 	@Override
 	public boolean onPreferenceChange(Preference p, Object o)
@@ -108,12 +127,22 @@ public class Main extends PreferenceActivity implements OnPreferenceChangeListen
 
 				//Set Intent Extras
 				awayService.putExtra("extraMessageContent", getMessageContent());
+				awayService.putExtra("extraInformStatus", getInformStatus());
 				awayService.putExtra("extraDelayDuration", Integer.toString(getDelayDuration()));
+				awayService.putExtra("extraRepeatStatus", getRepeatStatus());
 
 				//Start service and terminate activity
 				startService(awayService);
 				finish();
 			}
+		}
+		else if(p.getKey().equals(SILENT_PREF))
+		{
+			if(prefs.getBoolean(SILENT_PREF, false))
+				aManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+			else
+				aManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 		}
 		else if(p.getKey().equals(MESSAGE_PREF))
 			if(getMessageContent().equals(""))
@@ -149,23 +178,34 @@ public class Main extends PreferenceActivity implements OnPreferenceChangeListen
 	private void setPreferenceStatus(boolean status)
 	{
 		messageEditText.setEnabled(status);
+		informCheckBox.setEnabled(status);
 		delayEditText.setEnabled(status);
+		repeatCheckBox.setEnabled(status);
 	}
 	
 	//Getters and Setters for non-final variables
 	//Sets private variables AND preference
-	public boolean serviceRunning()							{ return serviceRunning;								}
-	public void setServiceStatus(boolean serviceRunning)	{ this.serviceRunning = serviceRunning;	
-															  editor.putBoolean(SERVICE_PREF, serviceRunning);	
-															  editor.commit();										}
+	public boolean serviceRunning()							{ return prefs.getBoolean(SERVICE_PREF, false);									}
+	public void setServiceStatus(boolean serviceRunning)	{ editor.putBoolean(SERVICE_PREF, serviceRunning);	
+															  editor.commit();																}
 
-	public String getMessageContent() 						{ return messageContent;								}
-	public void setMessageContent(String messageContent)	{ this.messageContent = messageContent;
-															  editor.putString(MESSAGE_PREF, messageContent);
-															  editor.commit();										}
+	public boolean isSilent()								{ return prefs.getBoolean(SILENT_PREF, false);									}
+	public void setSilentStatus(boolean isSilent)			{ editor.putBoolean(SILENT_PREF, isSilent);	
+															  editor.commit();																}
 	
-	public int getDelayDuration()							{ return delayDuration;									}
-	public void setDelayDuration(String delayDuration)		{ this.delayDuration = Integer.parseInt(delayDuration);				
-															  editor.putString(DELAY_PREF, delayDuration);
-															  editor.commit();										}
+	public String getMessageContent() 						{ return prefs.getString(MESSAGE_PREF, r.getString(R.string.message_content));	}
+	public void setMessageContent(String messageContent)	{ editor.putString(MESSAGE_PREF, messageContent);
+															  editor.commit();																}
+	
+	public boolean getInformStatus()						{ return prefs.getBoolean(INFORM_PREF, true);									}
+	public void setInformStatus(boolean informStatus)		{ editor.putBoolean(INFORM_PREF, informStatus);
+															  editor.commit();																}
+	
+	public int getDelayDuration()							{ return Integer.parseInt(prefs.getString(DELAY_PREF, "30"));					}
+	public void setDelayDuration(String delayDuration)		{ editor.putString(DELAY_PREF, delayDuration);
+															  editor.commit();																}
+	
+	public boolean getRepeatStatus()						{ return prefs.getBoolean(REPEAT_PREF, true);									}
+	public void setRepeatStatus(boolean repeatStatus)		{ editor.putBoolean(REPEAT_PREF, repeatStatus);
+															  editor.commit();																}
 }
